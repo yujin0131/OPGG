2bacad7ce7391b25a0b9c9a859f7e85472652133
lyujin0131 <yujin@DESKTOP-286L87U.localdomain>
2022-03-03 09:23:42 +0900

    Create Project

---
+++ src/test/java/com/yujin/opgg/OpggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujin.opgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
